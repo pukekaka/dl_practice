66d2626400243382f1de4d48273979a50f04da69
pukekakaster <pukekakaster@gmail.com>
2017-03-03 15:14:59 +0900

    dl_practice init commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+arr = np.array([1, 2, 3, 4, 5])
+#print(arr.dtype)
+
+float_arr = arr.astype(np.float64)
+#print(float_arr.dtype)
+
+test = np.array([3.7, -1.2, 0.5, 10.1])
+test2 = test.astype(np.int32)
+#print(test2)
+
+arr = np.array([[1., 2., 3.], [4., 5., 6.]])
+print(arr)
+print(arr*0.5)
+print(arr**0.5)
