3cb0c5fd81dee23b5dd13aac43027c9baa1cf99f
pukekakaster <pukekakaster@gmail.com>
2017-03-06 17:47:13 +0900

    Chapter4 ING...

--- test.py
+++ test.py
@@ -1,23 +1,38 @@
 import numpy as np
 
+'''
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
 
-arr = np.array([1, 2, 3, 4, 5])
-#print(arr.dtype)
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(mean_squared_error(np.array(y), np.array(t)))
+'''
+'''
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-float_arr = arr.astype(np.float64)
-#print(float_arr.dtype)
+s = np.sum(test)
+print(s)
+'''
 
-test = np.array([3.7, -1.2, 0.5, 10.1])
-test2 = test.astype(np.int32)
-#print(test2)
+'''
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+'''
 
-arr = np.array([[1., 2., 3.], [4., 5., 6.]])
-print(arr)
-print(arr*0.5)
-print(arr**0.5)
 
-tt = np.array([[1, 2, 3], [4, 5, 6]])
-print(tt.shape)
-print(len(tt))
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
 
-print("test")
+    batch_size = y.shape[0]
+    #return -np.sum(t * np.log(y)) / batch_size
+    return -np.sum(np.log(y[np.arrange(batch_size), t])) / batch_size
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t)))
+
+
