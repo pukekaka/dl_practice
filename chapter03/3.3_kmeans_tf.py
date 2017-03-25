2fe094477869b59a377489bb6e56ec5718655371
pukekakaster <pukekakaster@gmail.com>
2017-03-25 20:44:31 +0900

    k-means by ml

--- chapter03/3.3_kmeans_tf.py
+++ chapter03/3.3_kmeans_tf.py
@@ -21,7 +21,7 @@ df = pd.DataFrame({"x": [v[0] for v in vector_values],
                    "y": [v[1] for v in vector_values]})
 
 sns.lmplot("x", "y", data=df, fit_reg=False, size=7)
-plt.show()
+#plt.show()
 
 vectors = tf.constant(vector_values)
 centroids = tf.Variable(tf.slice(tf.random_shuffle(vectors), [0, 0], [num_clusters, -1]))
@@ -32,17 +32,39 @@ expanded_centroids = tf.expand_dims(centroids, 1)
 print(expanded_vectors.get_shape())
 print(expanded_centroids.get_shape())
 
-assignments = tf.argmin(tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroides)), 2), 0)
+distances = tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroids)), 2)
+assignments = tf.argmin(distances, 0)
 
-means = tf.concat(0, [tf.reduce_mean(tf.gather(vectors, tf.reshape(tf.where(tf.equal(assignments, c)), [1, -1])), reduction_indices=[1]) for c in range(k)])
+means = tf.concat([
+    tf.reduce_mean(
+        tf.gather(vectors,
+                  tf.reshape(
+                      tf.where(
+                          tf.equal(assignments, c)
+                      ), [1, -1])
+                  ), reduction_indices = [1])
+    for c in range(num_clusters)], 0)
 
-update_centroides = tf.assign(centroides, means)
+update_centroids = tf.assign(centroids, means)
 
-init_op = tf.initialize_all_variables()
+init_op = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init_op)
 
-for step in range(100):
-    _, centroid_values, assignment_values = sess.run([update_centroides, centroides, assignments])
+for step in range(num_steps):
+    _, centroid_values, assignment_values = sess.run([update_centroids, centroids, assignments])
 
+print("centroids")
+print(centroid_values)
+
+data = {"x": [], "y": [], "cluster": []}
+for i in range(len(assignment_values)):
+    data["x"].append(vector_values[i][0])
+    data["y"].append(vector_values[i][1])
+    data["cluster"].append(assignment_values[i])
+df = pd.DataFrame(data)
+sns.lmplot("x", "y", data=df,
+           fit_reg=False, size=7,
+           hue="cluster", legend=False)
+plt.show()
