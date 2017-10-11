072c2ff1a8b8dd5a7d9ffa00d8d904677e1423b8
pukekaka <pukekakaster@gmail.com>
2017-10-11 10:32:33 +0900

    organizing directory

--- test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-dic = {'name':'pey', 'phone':'0119993323', 'birth': '1118'}
-print (dic.keys())
---
+++ test_code/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+dic = {'name':'pey', 'phone':'0119993323', 'birth': '1118'}
+print (dic.keys())
