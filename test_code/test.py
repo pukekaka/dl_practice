0b9a24daafa8ec4a4abfa9cc3204135f70faa231
pukekaka <pukekakaster@gmail.com>
2017-10-15 23:46:22 +0900

    ing - kmeans

--- test_code/test.py
+++ test_code/test.py
@@ -1,11 +1,16 @@
 
 
-
-
-
-
-
 '''
+def make_forge():
+    X, y = make_blobs(centers=2, random_state=4, n_samples=30)
+        y[np.array([7, 27])] = 0
+        mask = np.ones(len(X), dtype=np.bool)
+        mask[np.array([0, 1, 5, 26])] = 0
+        X, y = X[mask], y[mask]
+        return X, y
+
+
+
 numFeat = len(open("abalone.txt").readline().split('\t')) - 1
 dataMat = []; labelMat = []
 fr = open("abalone.txt")
